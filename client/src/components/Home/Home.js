7e1a4d60934d3a10102a1f55f97cd907e9273500
Aman Chopra <amanchopra954@gmail.com>
2020-09-17 19:29:53 +0530

    push

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Grid, Paper } from '@material-ui/core'
 
 const Home = (props) => {
     return(
-        <div>
-            
-        </div>
+        <Grid container>
+            <Grid item>
+                <Paper>Dashboard</Paper>
+            </Grid>
+            <Grid item>
+                <Paper>Form</Paper>
+            </Grid>
+        </Grid>
     )
 }
 
