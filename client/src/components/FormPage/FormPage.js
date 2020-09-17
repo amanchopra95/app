f12d02de46acb4dbce09539ae6991ddc08a53680
Aman Chopra <amanchopra954@gmail.com>
2020-09-17 22:11:12 +0530

    toggle

--- client/src/components/FormPage/FormPage.js
+++ client/src/components/FormPage/FormPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cls from './FormPage.module.css'
 
 import Container from '@material-ui/core/Container'
@@ -9,11 +9,60 @@ import CardContent from '@material-ui/core/CardContent'
 import TextField from '@material-ui/core/TextField'
 import FormLabel from '@material-ui/core/FormLabel'
 import RadioGroup from '@material-ui/core/RadioGroup'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormGroup from '@material-ui/core/FormGroup'
 import InputLayout from './InputLayout'
+import { Checkbox } from '@material-ui/core'
 
 const FormPage = (props) => {
+
+    const [value, setValue] = useState(true)
+    const [show, setShow] = useState(null)
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+        if (e.target.value) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
+    const input = () => {
+        if (show == null) {
+            return null;
+        } else if (show) {
+            return (
+                <Grid item col={12}>
+                    <InputLayout tooltip="Please select Yes or No">
+                        <FormLabel>Input Box</FormLabel>
+                        <RadioGroup row>
+                            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
+                            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
+                        </RadioGroup>
+                    </InputLayout>
+                </Grid>
+            )
+        } else {
+            return (
+                <Grid item col={12}>
+                    <InputLayout tooltip="Please select Yes or No">
+                        <FormLabel>Input Box</FormLabel>
+                        <FormGroup row>
+                            <FormControlLabel value="Yes" control={<Checkbox color="primary" />} label="Yes" />
+                            <FormControlLabel value="No" control={<Checkbox color="primary" />} label="No" />
+                        </FormGroup>
+                    </InputLayout>
+                </Grid>
+            )
+        }
+    }
+
 
     return (
         <Container style={{ 'height': '100vh', 'padding': '15px' }}>
@@ -32,15 +81,6 @@ const FormPage = (props) => {
                                     </InputLayout>
                                 </Grid>
                                 <Grid item col={12}>
-                                    <InputLayout tooltip="Please select Yes or No">
-                                        <FormLabel>Input Box</FormLabel>
-                                        <RadioGroup row>
-                                            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
-                                            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
-                                        </RadioGroup>
-                                    </InputLayout>
-                                </Grid>
-                                <Grid item col={12}>
                                     <InputLayout tooltip="Please enter Index of Label Column">
                                         <TextField 
                                             helperText="Label Column"
@@ -55,6 +95,26 @@ const FormPage = (props) => {
                                         <TextField helperText="Wowo"/>
                                     </InputLayout>
                                 </Grid>
+                                <Grid item col={12}>
+                                    <InputLayout tooltip="Please enter index of label column">
+                                        <FormControl>
+                                            <InputLabel htmlFor="true-false">Select Input</InputLabel>
+                                            <Select
+                                                labelId="true-false"
+                                                id="true-false"
+                                                name="Select Input"
+                                                value={value}
+                                                onChange={handleChange}
+                                                >
+                                                <MenuItem value={true}>True</MenuItem>
+                                                <MenuItem value={false}>False</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </InputLayout>
+                                </Grid>
+                                {
+                                    input()
+                                }
                             </Grid>
                         </CardContent>
                     </Card>
