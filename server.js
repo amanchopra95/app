c396a5e0978ab7572252f5c28bcc8556afc1f670
Aman Chopra <amanchopra954@gmail.com>
2020-09-17 19:51:10 +0530

    PORT

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () => {
     console.log("Server running in " + process.env.NODE_ENV + " mode on port " + PORT)
